32c8a7b75dcf7e9913621550ee3b9489d06f0613
Eric Hornby <erich@frost-fighter.local>
2019-10-16 16:03:01 -0500

    initial commit

---
+++ hn-client/src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { getStoryIds } from './services/hnAPI';
+
+export const App = () => {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then(data => setStoryIds(data));
+  }, []);
+  
+  return (
+    <p>{JSON.stringify(storyIds)}</p>
+  );
+};
+
